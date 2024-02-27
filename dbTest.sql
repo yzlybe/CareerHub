2b023c504ce72d3a339644e7da23ef6d0d8fc7ba
jarajiri <eic3021@gmail.com>
2024-02-27 20:12:32 +0900

    123

--- dbTest.sql
+++ dbTest.sql
@@ -29,3 +29,32 @@ BEGIN
 END;
 -- 프로시저 실행
 CALL random_insert(100);
+
+-- Jobs
+drop table if EXISTS jobs; -- users 테이블이 이미 존재할 경우 삭제
+CREATE TABLE jobs (
+    jobs_id INT PRIMARY KEY AUTO_INCREMENT,
+    users_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATE 
+    nickname VARCHAR(20)
+);
+desc users;
+insert into users VALUES (null,"test@naver.com","1234",default,"jarajiri");
+SELECT * from users;
+-- 더미 데이터 삽입 프로시저
+DROP procedure IF EXISTS `random_insert`;
+CREATE PROCEDURE random_insert(IN num_rows INT)
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    
+    WHILE i <= num_rows DO
+        INSERT INTO users (users_email, users_password, created_at, nickname)
+        VALUES (CONCAT('user', i, '@example.com'), (CONCAT('password', i)), CURRENT_DATE, CONCAT('User', i));
+        
+        SET i = i + 1;
+    END WHILE;
+    
+END;
+-- 프로시저 실행
+CALL random_insert(100);
