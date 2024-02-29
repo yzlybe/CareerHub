614ea3d371074cb3507649acdf304a9a9223963d
jarajiri <eic3021@gmail.com>
2024-02-29 21:40:49 +0900

    구글 로그인 테스트

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -5,6 +5,7 @@ const {
     reviewsModel,
     likesModel,
 } = require("../models");
+const dotenv = require("dotenv").config();
 
 exports.index = (req, res) => {
     res.render("index");
@@ -172,4 +173,33 @@ exports.logout = async (req, res) => {
         if (err) throw err;
     });
     res.redirect("/");
+};
+
+// =================== oAuth ===================
+
+exports.googleTest = (req, res) => {
+    res.render("gLogin");
+};
+
+exports.googleLogin = (req, res) => {
+    let url = "https://accounts.google.com/o/oauth2/v2/auth";
+    // client_id는 위 스크린샷을 보면 발급 받았음을 알 수 있음
+    // 단, 스크린샷에 있는 ID가 아닌 당신이 직접 발급 받은 ID를 사용해야 함.
+    url += `?client_id=${process.env.GOOGLE_CLIENT_ID}`;
+    // 아까 등록한 redirect_uri
+    // 로그인 창에서 계정을 선택하면 구글 서버가 이 redirect_uri로 redirect 시켜줌
+    url += `&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}`;
+    // 필수 옵션.
+    url += "&response_type=code";
+    // 구글에 등록된 유저 정보 email, profile을 가져오겠다 명시
+    url += "&scope=email profile";
+    // 완성된 url로 이동
+    // 이 url이 위에서 본 구글 계정을 선택하는 화면임.
+    res.redirect(url);
+};
+
+exports.googleLoginDone = (req, res) => {
+    const { code } = req.query;
+    console.log(`code: ${code}`);
+    res.send("login ok");
 };
--- routes/router.js
+++ routes/router.js
@@ -16,7 +16,11 @@ router.get("/mypage", mainCtr.findUserProfile);
 router.put("/mypage", mainCtr.updateUser);
 router.delete("/mypage", mainCtr.deleteUser);
 router.get("/logout", mainCtr.logout);
-
+// 테스트용 gLogin.ejs 페이지 렌더링 경로
+router.get("/google/test", mainCtr.googleTest);
+// 구글 인증 서버로 리다이렉트 될 경로
+router.get("/google/login", mainCtr.googleLogin);
+router.get("/google/success", mainCtr.googleLoginDone);
 /*  ========jobs routing========= 
 
 */
