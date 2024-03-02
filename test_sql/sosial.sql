135c20f0080292db458b89465a9f687cd3b5625e
jarajiri <140472588+jarajiri@users.noreply.github.com>
2024-03-02 19:28:32 +0900

    Delete test_sql/sosial.sql

--- test_sql/sosial.sql
+++
@@ -1,5 +0,0 @@
--- Active: 1707101283311@@127.0.0.1@3306@sesac
-select * from users;
-insert into users values(null,'eic40211@gmail.com','google login user',default,'nickname');
-
-DELETE from users where users_email = 'eic3021@gmail.com';
