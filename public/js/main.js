f4006b8e79dde8b3218ca44ccba7f90f7986f96e
jarajiri <eic3021@gmail.com>
2024-03-11 10:25:48 +0900

    모달 수정

--- public/js/main.js
+++ public/js/main.js
@@ -389,15 +389,20 @@ document.addEventListener("DOMContentLoaded", function () {
         <form id="loginForm">
           <input type="email" name="email" placeholder="이메일 주소" required><br>
           <input type="password" name="password" placeholder="비밀번호" required><br>
-          <button type="submit" class="login-action">로그인</button>          
+          <button type="submit" class="login-action">로그인</button>
+          
 
-          <button type="button" onclick="renderSignupForm()">회원 가입</button>
-          <a href="/google/login">
-          <img src="/static/img/google.png" alt="Google 로그인" style="margin-top: 5px;" height="55px" width="293px">
-        </a>
-        <a href="/google/signup">
-          <img src="/static/img/google.png" alt="Google 회원가입" style="margin-top: 5px;" height="55px" width="293px">
-        </a>
+          <a href="구글 로그인 링크"> <button type="button" style="background-color: #fff; color: #222;"><img src="/static/img/111.png" alt="Google 로그인" style="width: 20px;" "align-items: center;">  sign in with Google</button></a>
+
+<button type="button" onclick="renderSignupForm()" style="background-color: #fff; color: #000; font-size: 12px;">
+    회원이 아니신가요? <span style="font-size: 12px; text-decoration: underline; font-weight: 700;">회원 가입</span>
+</button>
+
+
+
+
+
+
           </form>
       `;
         // 로그인 폼 submit 이벤트 처리
